deec256d544cf66532fa90c9578817ceb93e8a42
Gregory A. Kosinovsky <kosinov@quartz386.llnl.gov>
2020-08-26 14:39:23 -0700

    changed default in .cmake function

--- cmake/RANKSTR_ADD_TEST.cmake
+++ cmake/RANKSTR_ADD_TEST.cmake
@@ -2,7 +2,7 @@ function(RANKSTR_ADD_TEST name args outputs)
 
   # job launcher
   if(NOT DEFINED VELOC_RESOURCE_MANAGER)
-    set(test_param srun -N 3 -n 3)
+    set(test_param mpirun -np 3)
   elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "NONE")
     set(test_param mpirun -np 3)
   elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "LSF")
